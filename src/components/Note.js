2d28cfb7df74bb6a158b346e649da972df02da7a
MERAJ HASSAN <101815583+merajafnan@users.noreply.github.com>
2023-12-22 12:08:13 +0530

    comm

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="Note">
+            <h2>This is Title</h2>
+            <p>This is content</p>
+        </div>
+    );
+}
+
+export default Note;
